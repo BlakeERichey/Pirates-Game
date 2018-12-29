a117f797f6ed2caf17e5482931ca81d956928aa1
Blake <blake.e.richey@gmail.com>
2018-12-29 13:44:59 -0600

    Ships are being generated and able to move by clicking.

--- Pirates.py
+++ Pirates.py
@@ -1,42 +1,91 @@
 def Pirates():   
     ##GUI Main Game
     import pygame, time, sys
+    import ShipClass
+    from   ShipClass import Ship
+    
     gamePadCoordinate = ""
 
     def run_game():
         pygame.init()
 
-        #black = (0, 0, 0)
-        #white = (255, 255, 255)
-        #red = (255, 0, 0)
-        #green = (0, 255, 0)
-        #blue = (0, 0, 255)
-        background = (51, 70, 242)
-        battlefieldBackground=pygame.image.load('./resources//images/background-battlefield.jpg')
+        #background = (51, 70, 242)
+        battlefieldBackground=pygame.image.load('./resources/images/background-battlefield.jpg')
+
+        #Sprites Resources
+        galleySprite = pygame.image.load('./resources//images/Icon_Galley.png')
+        schSprite    = pygame.image.load('./resources//images/Icon_Schooner.png')
 
         display_width = 1920
         display_height = 1080
 
-        gameDisplay = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+        #Load and Fill Background
+        gameDisplay = pygame.display.set_mode((display_width, display_height), pygame.FULLSCREEN)
         pygame.display.set_caption('Pirates PC!')
         clock = pygame.time.Clock()
-        gameDisplay.fill(background)
+        #gameDisplay.fill(background)
         gameDisplay.blit(battlefieldBackground, (0,0))
+
+        #Set Player Ships
+        x = Ship("Schooner")
+        x.pos=(3,3)
+        Player1Ships = [x]    
+        
+        #Load Sprites
+        for ship in Player1Ships:
+          if(ship.type == "Galley"):
+              gameDisplay.blit(galleySprite, ship.getPosition())
+          if(ship.type == "Schooner"):
+              gameDisplay.blit(schSprite, ship.getPosition())
+
         pygame.display.update()
 
+        #Check for events
         while True:
+        
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              pygame.quit()
+              quit()
+
+            #Monitor when keyboard key is pressed
+            if event.type == pygame.KEYDOWN:
+              if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+
+            #Monitor when mouse is pressed
+            if event.type == pygame.MOUSEBUTTONDOWN:
+              #get position of mouse and save it ss mx and my
+              mx, my = pygame.mouse.get_pos()
             
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+              for x in Player1Ships:
+                print((x.pos[0] - 1)*64)
+                print(mx)
+                if (x.pos[0] - 1)*64<mx and x.pos[0]*64>mx:
+                  print("You hit!")
+              
+            if event.type == pygame.MOUSEBUTTONUP:
+              print("key lifted")
+              newMx, newMy = pygame.mouse.get_pos()
+              
+              Player1Ships[0].setPosition((newMx, newMy))
+              print(Player1Ships[0].pos)
 
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.quit()
-                        exit()
+          #Load and Fill Background
+          gameDisplay.blit(battlefieldBackground, (0,0))
 
-            time.sleep(0.03)
+          #Load Sprites
+          for ship in Player1Ships:
+            if(ship.type == "Galley"):
+                gameDisplay.blit(galleySprite, ship.getPosition())
+            if(ship.type == "Schooner"):
+                gameDisplay.blit(schSprite, ship.getPosition())
+          
+          #Rerender
+          pygame.display.update()
+
+          time.sleep(0.03)
     run_game()
 
 Pirates()
--- ShipClass.py
+++ ShipClass.py
@@ -1,5 +1,8 @@
 class Ship():
-##    shipType = ""
+##    type = ""
+##    dir = "up"
+##    pos
+##    owner
 ##    damage = -1
 ##    hp = -1
 ##    masts = -1
@@ -10,28 +13,45 @@ class Ship():
 ##    sight = -1
 
     def __init__(self, ship):
-        self.shipType=ship
-        if(ship == "Galley"):
-           self.damage=self.hp=self.masts=self.aRange=self.accuracy=self.speed=self.carry=self.sight=3
-        elif(ship=="Cargo"):
-            self.damage=1
-            self.aRange=self.accuracy=self.speed=2
-            self.masts=self.sight=3
-            self.hp=4
-            self.carry=5
-        elif(ship == "Schooner"):
-           self.damage=self.carry=1
-           self.hp=self.masts=self.aRange=2
-           self.accuracy=3
-           self.speed=self.sight=5
-        elif(ship=="Frigate"):
-            self.damage=self.aRange=self.accuracy=self.sight=4
-            self.hp=self.masts=3
-            self.speed=self.carry=2
+      self.type=ship
+      self.dir="up"
+      self.pos = (0,0)
+      self.owner="Player1"
+      if(ship == "Galley"):
+        self.damage=self.hp=self.masts=self.aRange=self.accuracy=self.speed=self.carry=self.sight=3
+      elif(ship=="Cargo"):
+        self.damage=1
+        self.aRange=self.accuracy=self.speed=2
+        self.masts=self.sight=3
+        self.hp=4
+        self.carry=5
+      elif(ship == "Schooner"):
+        self.damage=self.carry=1
+        self.hp=self.masts=self.aRange=2
+        self.accuracy=3
+        self.speed=self.sight=5
+      elif(ship=="Frigate"):
+        self.damage=self.aRange=self.accuracy=self.sight=4
+        self.hp=self.masts=3
+        self.speed=self.carry=2
             
     def __str__(self):
-        rv = "Ship:\nType: " + str(self.shipType) + "\nDamage: " + str(self.damage)
-        rv += "\nHp: " + str(self.hp) + "\nMasts: " + str(self.masts)
-        rv += "\nRange: " + str(self.aRange) + "\nAccuracy: " + str(self.accuracy)
-        rv += "\nSpeed: " + str(self.speed) + "\nCarry " + str(self.carry) + "\nSight: " + str(self.sight)
-        return rv
+      rv = "Ship:\nType: " + str(self.type) + "\nDirection: " + str(self.dir)
+      rv += "\nPosition: " + str(self.pos) + "\nOwner: " + str(self.owner)
+      rv += "\nDamage: " + str(self.damage)
+      rv += "\nHp: " + str(self.hp) + "\nMasts: " + str(self.masts)
+      rv += "\nRange: " + str(self.aRange) + "\nAccuracy: " + str(self.accuracy)
+      rv += "\nSpeed: " + str(self.speed) + "\nCarry " + str(self.carry) + "\nSight: " + str(self.sight)
+      return rv
+    
+    def getPosition(self):
+      x = (self.pos[0] - 1) * 64
+      y = (self.pos[1] - 1) * 64
+      return (x, y)
+    
+    #Accepts tuple of new coodinate in pixel form and transforms it to a 
+    #grid coordinate then sets the ships location
+    def setPosition(self, newPos):
+      x = int(newPos[0]/64) + 1
+      y = int(newPos[1]/64) + 1
+      self.pos = (x,y)
